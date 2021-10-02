bcf12c5fda6404f71aa49d2c23bec584edb19c6a
Kalle Hirvola <khirvola@gmail.com>
2021-10-02 16:23:23 +0200

    Add temporary max file size

--- client/src/services/useUpload.ts
+++ client/src/services/useUpload.ts
@@ -7,12 +7,18 @@ import { uploadedFileState } from "../state/uploadedFileState";
 import { uploadProgresState } from "../state/uploadProgresState";
 import { getServerUrl } from "../utils/url";
 
+const MAX_FILE_SIZE = 10_000_000;
+
 export function useUpload() {
   const [, setLocation] = useLocation();
   const setUploadedFile = useSetRecoilState(uploadedFileState);
   const setUploadProgress = useSetRecoilState(uploadProgresState);
 
   const upload = useCallback(async (file: File) => {
+    if (file.size > MAX_FILE_SIZE) {
+      return alert("Max koko 10MB");
+    }
+
     const formData = new FormData();
     formData.append("file", file);
 
