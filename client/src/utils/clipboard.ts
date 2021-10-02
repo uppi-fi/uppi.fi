aa3e2b7d2d255db6243f68c804ddf32ded625b43
Kalle Hirvola <khirvola@gmail.com>
2021-10-02 16:23:44 +0200

    Add fallback copy to clipboard method

--- client/src/utils/clipboard.ts
+++ client/src/utils/clipboard.ts
@@ -1,9 +1,25 @@
 export async function copyToClipboard(text: string) {
-  try {
-    await navigator.clipboard.writeText(text);
-    return true;
-  } catch (err) {
-    console.error("Failed to copy text to clipboard: ", err);
-    return false;
+  if (navigator.clipboard) {
+    try {
+      await navigator.clipboard.writeText(text);
+      return true;
+    } catch {
+      return false;
+    }
   }
+
+  // "document.execCommand" fallback method
+  const textArea = document.createElement("textarea");
+  textArea.value = text;
+
+  // Move out of viewport
+  textArea.style.position = "fixed";
+  textArea.style.left = "-999999px";
+  textArea.style.top = "-999999px";
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+  const wasCopied = document.execCommand("copy");
+  textArea.remove();
+  return wasCopied;
 }
