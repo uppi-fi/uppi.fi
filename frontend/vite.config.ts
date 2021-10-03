4d51badd1bc2c44ae45965d746dd38b3ac1e58aa
Kalle Hirvola <khirvola@gmail.com>
2021-10-03 18:09:52 +0300

    Vite config changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,16 +2,14 @@ import react from "@vitejs/plugin-react";
 import * as path from "path";
 import { defineConfig } from "vite";
 
-const frontendPath = path.join(process.cwd(), "frontend");
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: frontendPath,
+  root: __dirname,
   server: {
     fs: {
       strict: false,
       // Allow serving files from one level up to the project root
-      allow: [frontendPath],
+      allow: [path.resolve(__dirname, "..")],
     },
   },
   plugins: [react()],
@@ -20,6 +18,14 @@ export default defineConfig({
       {
         find: "@shared",
         replacement: path.resolve(__dirname, "../shared"),
+      },
+      {
+        find: "@backend",
+        replacement: path.resolve(__dirname, "../backend"),
+      },
+      {
+        find: "@frontend",
+        replacement: __dirname,
       },
     ],
   },
