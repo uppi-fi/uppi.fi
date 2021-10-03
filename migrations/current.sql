9d7bf609881bf3f55b4bc286e7fa743412f394d7
Kalle Hirvola <khirvola@gmail.com>
2021-10-03 17:34:17 +0300

    Fix file id column length

--- migrations/current.sql
+++ migrations/current.sql
@@ -1,6 +1,5 @@
 -- Users
-DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users  (
+CREATE TABLE IF NOT EXISTS users  (
   user_id VARCHAR(36) PRIMARY KEY UNIQUE,
   telegram_user_id BIGINT UNIQUE,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
@@ -9,7 +8,7 @@ CREATE TABLE users  (
 -- Files
 DROP TABLE IF EXISTS files;
 CREATE TABLE files  (
-  id  VARCHAR(5) PRIMARY KEY,
+  id  VARCHAR(10) PRIMARY KEY,
   filename VARCHAR(255) NOT NULL,
   mime_type VARCHAR(127) NOT NULL,
   custom_name VARCHAR(255),
