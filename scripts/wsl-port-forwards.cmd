7a641bad7b9fa02f4377f4d2e01571c470995da5
Kalle Hirvola <khirvola@gmail.com>
2021-10-06 20:10:02 +0300

    Add WSL port-forward scripts

--- scripts/wsl-port-forwards.cmd
+++ scripts/wsl-port-forwards.cmd
@@ -1,7 +1,7 @@
 @echo off
 for /f %%i in ('wsl hostname -I') do set WSL_IP=%%i
-netsh interface portproxy delete v4tov4 listenport=3005 listenaddress=%WSL_IP%
-netsh interface portproxy delete v4tov4 listenport=5433 listenaddress=%WSL_IP%
-netsh interface portproxy delete v4tov4 listenport=8000 listenaddress=%WSL_IP%
+netsh interface portproxy add v4tov4 listenport=3005 listenaddress=0.0.0.0 connectport=3005 connectaddress=%WSL_IP%
+netsh interface portproxy add v4tov4 listenport=5433 listenaddress=0.0.0.0 connectport=5433 connectaddress=%WSL_IP%
+netsh interface portproxy add v4tov4 listenport=8000 listenaddress=0.0.0.0 connectport=8000 connectaddress=%WSL_IP%
 netsh interface portproxy show all
 pause
