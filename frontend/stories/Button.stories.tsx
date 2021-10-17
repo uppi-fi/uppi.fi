e7d51efc5d046ad479d19d42b6273c3bf56a304c
naftis <pyry.rouvila@gmail.com>
2021-10-17 19:33:35 +0300

    Add couple of buttons to storybook

--- frontend/stories/Button.stories.tsx
+++ frontend/stories/Button.stories.tsx
@@ -1,9 +1,14 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import Button from '../src/components/Button';
+import BackButton from '../src/components/BackButton';
+import CopyButton from '../src/components/CopyButton';
+import IconButton from '../src/components/IconButton';
+import DownloadButton from '../src/components/DownloadButton';
+import DeleteButton from '../src/components/DeleteButton';
 
 export default {
-  title: 'Example/Button',
+  title: 'Button',
   component: Button,
 } as ComponentMeta<typeof Button>;
 
@@ -20,3 +25,15 @@ export const Secondary = Template.bind({});
 Secondary.args = {
   kind: 'secondary',
 };
+
+export const Back = () => <BackButton>Go back</BackButton>;
+
+export const Copy = () => (
+  <CopyButton textToCopy="This is now on your clipboard" />
+);
+
+export const Delete = () => <DeleteButton fileId="0" />;
+
+export const Icon = () => <IconButton icon="ant-design:file-twotone" />;
+
+export const Download = () => <DownloadButton url="https://google.fi" />;
