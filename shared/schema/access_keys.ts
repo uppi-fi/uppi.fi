6997bd6116dc2f97641571a6653e655bab102a2b
Kalle <khirvola@gmail.com>
2021-10-31 14:52:07 +0200

    Set 500MB as nginx max body size (#59)

--- shared/schema/access_keys.ts
+++ shared/schema/access_keys.ts
@@ -6,9 +6,14 @@ export type AccessKeyTId = string & { " __flavor"?: 'access_keys' };
 export default interface AccessKeyT {
   /** Primary key. Index: access_keys_pkey */
   accessKey: AccessKeyTId;
+
+  useCount: number;
 }
 
 export interface AccessKeyTInitializer {
   /** Primary key. Index: access_keys_pkey */
   accessKey: AccessKeyTId;
+
+  /** Default value: 0 */
+  useCount?: number;
 }
